440fa7acfacc3769b206a2ac5636e5aac434e7fd
lunifo <157994627+lunifo@users.noreply.github.com>
2026-04-08 18:21:56 +0200

    Make stage decouplers wrenchable and nicer to place

--- common/src/main/java/io/github/lunifo/finalfrontier/block/StageDecouplerBlock.java
+++ common/src/main/java/io/github/lunifo/finalfrontier/block/StageDecouplerBlock.java
@@ -1,19 +1,40 @@
 package io.github.lunifo.finalfrontier.block;
 
+import com.simibubi.create.foundation.block.WrenchableDirectionalBlock;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class StageDecouplerBlock extends DirectionalBlock {
+import java.util.Objects;
+
+public class StageDecouplerBlock extends WrenchableDirectionalBlock {
 	protected StageDecouplerBlock(Properties properties) {
 		super(properties);
 		registerDefaultState(defaultBlockState().setValue(FACING, Direction.UP));
 	}
 
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(FACING);
+	public @Nullable BlockState getStateForPlacement(@NotNull BlockPlaceContext blockPlaceContext) {
+		BlockState state = Objects.requireNonNull(super.getStateForPlacement(blockPlaceContext), "We're most likely dealing with some incautious modder.");
+		BlockPos placementPos = blockPlaceContext.getClickedPos();
+		Level level = blockPlaceContext.getLevel();
+
+		Direction alignableDirection = null;
+		for (Direction direction : Direction.values()) {
+			BlockState neighbourState = level.getBlockState(placementPos.relative(direction));
+			if (neighbourState.is(this) && neighbourState.getValue(FACING).getOpposite() == direction) {
+				if (alignableDirection == null) {
+					alignableDirection = direction;
+				} else {
+					return state;
+				}
+			}
+		}
+
+		return alignableDirection == null ? state : state.setValue(FACING, alignableDirection);
 	}
 }
