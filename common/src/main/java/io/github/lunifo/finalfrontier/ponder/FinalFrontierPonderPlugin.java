66f251d0ca4f213c3dc5154f2990af32dd7c91c4
lunifo <157994627+lunifo@users.noreply.github.com>
2026-05-31 14:50:30 +0200

    Add basic assembly instructions to the ship controls ponder

--- common/src/main/java/io/github/lunifo/finalfrontier/ponder/FinalFrontierPonderPlugin.java
+++ common/src/main/java/io/github/lunifo/finalfrontier/ponder/FinalFrontierPonderPlugin.java
@@ -1,5 +1,6 @@
 package io.github.lunifo.finalfrontier.ponder;
 
+import com.simibubi.create.AllItems;
 import com.tterrag.registrate.util.entry.ItemProviderEntry;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import io.github.lunifo.finalfrontier.FinalFrontier;
@@ -12,9 +13,10 @@ import net.createmod.ponder.api.scene.SceneBuilder;
 import net.createmod.ponder.api.scene.SceneBuildingUtil;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
 import org.jetbrains.annotations.NotNull;
 
 @Environment(EnvType.CLIENT)
@@ -32,22 +34,65 @@ public class FinalFrontierPonderPlugin implements PonderPlugin {
 				.addStoryBoard("ship_controls", FinalFrontierPonderPlugin::shipControls);
 	}
 
-	private static void shipControls(SceneBuilder scene, SceneBuildingUtil buildingUtil) {
+	private static void shipControls(SceneBuilder scene, SceneBuildingUtil util) {
 		scene.title("ship_controls", "Using Ship Controls");
 		scene.configureBasePlate(0, 0, 5);
 		scene.showBasePlate();
+		scene.setSceneOffsetY(-2);
 
-		scene.world().showSection(buildingUtil.select().fromTo(1, 1, 2, 3, 11, 3), Direction.NORTH);
-
+		scene.idle(10);
+		scene.world().showSection(util.select().fromTo(1, 1, 2, 3, 11, 3), Direction.NORTH);
 		scene.idleSeconds(1);
 
+		// Glue rocket
 		scene.addKeyframe();
-		scene.overlay().showOutline(PonderPalette.GREEN, "Ship Controls Highlight", buildingUtil.select().position(2, 7, 2), 20);
-		scene.overlay().showControls(new Vec3(2.5, 8, 2.5), Pointing.DOWN, 20)
-				.rightClick();
 
-		scene.idleSeconds(2);
+		scene.overlay().showControls(util.vector().blockSurface(new BlockPos(1, 1, 2), Direction.WEST), Pointing.LEFT, 10)
+				.rightClick()
+				.withItem(AllItems.SUPER_GLUE.asStack());
+		scene.idle(5);
+		scene.effects().indicateSuccess(new BlockPos(1, 1, 2));
+		scene.idleSeconds(1);
+
+		AABB glueBox = new AABB(new BlockPos(1, 1, 2));
+		scene.overlay().chaseBoundingBoxOutline(PonderPalette.GREEN, "Rocket Glue", glueBox, 1);
+		scene.overlay().chaseBoundingBoxOutline(PonderPalette.GREEN, "Rocket Glue", glueBox.expandTowards(2, 10, 1), seconds(2));
+
+		scene.idle(10);
+
+		scene.overlay().showControls(util.vector().blockSurface(new BlockPos(3, 11, 3), Direction.EAST), Pointing.RIGHT, 10)
+				.rightClick()
+				.withItem(AllItems.SUPER_GLUE.asStack());
+		scene.idle(5);
+		scene.effects().indicateSuccess(new BlockPos(3, 11, 3));
+		scene.idleSeconds(1);
+
+		scene.overlay().showText(seconds(2.5))
+				.text("First glue your rocket together")
+				.colored(PonderPalette.GREEN)
+				.pointAt(util.vector().blockSurface(new BlockPos(1, 1, 2), Direction.WEST))
+				.placeNearTarget();
+
+		scene.idleSeconds(3);
+
+		// Assemble rocket
+		scene.addKeyframe();
+		scene.overlay().showOutline(PonderPalette.BLUE, "Ship Controls Highlight", util.select().position(2, 7, 2), seconds(5));
+		scene.overlay().showControls(util.vector().topOf(2, 7, 2), Pointing.DOWN, seconds(1))
+				.rightClick();
+		scene.idleSeconds(1);
+		scene.overlay().showText(seconds(4))
+				.text("Assemble the rocket by clicking the ship controls")
+				.colored(PonderPalette.BLUE)
+				.pointAt(util.vector().blockSurface(new BlockPos(2, 7, 2), Direction.NORTH))
+				.placeNearTarget();
+
+		scene.idleSeconds(5);
 
 		scene.markAsFinished();
+	}
+
+	private static int seconds(double ticks) {
+		return (int)(ticks * 20);
 	}
 }
