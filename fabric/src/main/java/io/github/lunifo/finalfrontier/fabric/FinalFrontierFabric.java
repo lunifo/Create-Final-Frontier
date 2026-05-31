0657a2d0b7e95fb358a5625e3879fcc1f7315b61
lunifo <157994627+lunifo@users.noreply.github.com>
2026-05-31 17:22:14 +0200

    Set up ponder language datagen and run it

--- fabric/src/main/java/io/github/lunifo/finalfrontier/fabric/FinalFrontierFabric.java
+++ fabric/src/main/java/io/github/lunifo/finalfrontier/fabric/FinalFrontierFabric.java
@@ -1,7 +1,10 @@
 package io.github.lunifo.finalfrontier.fabric;
 
+import com.tterrag.registrate.providers.ProviderType;
+import com.tterrag.registrate.providers.RegistrateLangProvider;
 import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
 import io.github.lunifo.finalfrontier.FinalFrontier;
+import net.createmod.ponder.foundation.PonderIndex;
 import net.fabricmc.api.ModInitializer;
 
 public class FinalFrontierFabric implements ModInitializer {
@@ -15,6 +18,12 @@ public class FinalFrontierFabric implements ModInitializer {
         // on fabric, Registrates must be explicitly finalized and registered.
         FinalFrontier.REGISTRATE.register();
 
+        FinalFrontier.REGISTRATE.addDataGenerator(ProviderType.LANG, FinalFrontierFabric::addPonderLang);
+
         FinalFrontier.registerCreateDependent();
+    }
+
+    private static void addPonderLang(RegistrateLangProvider provider) {
+        PonderIndex.getLangAccess().provideLang(FinalFrontier.MOD_ID, provider::add);
     }
 }
