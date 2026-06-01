b8b98b91cad03dff497bd5c47f8c4682f45e5883
lunifo <157994627+lunifo@users.noreply.github.com>
2026-06-01 18:05:57 +0200

    Add a deep space dimension which is completely empty and renders a space skybox

--- common/src/main/java/io/github/lunifo/finalfrontier/util/PlayerUtil.java
+++ common/src/main/java/io/github/lunifo/finalfrontier/util/PlayerUtil.java
@@ -1,16 +1,29 @@
 package io.github.lunifo.finalfrontier.util;
 
+import io.github.lunifo.finalfrontier.FinalFrontier;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class PlayerUtil {
-	public static double SPACE_TRANSITION_START = 256;
-	public static double SPACE_TRANSITION_END = 2048;
-	public static double SPACE_TRANSITION_LENGTH = SPACE_TRANSITION_END - SPACE_TRANSITION_START;
+	public static final double SPACE_TRANSITION_START = 256;
+	public static final double SPACE_TRANSITION_END = 2048;
+	public static final double SPACE_TRANSITION_LENGTH = SPACE_TRANSITION_END - SPACE_TRANSITION_START;
+
+	// This should be moved out of here
+	private static final ResourceKey<Level> DEEP_SPACE = ResourceKey.create(Registries.DIMENSION, new ResourceLocation(FinalFrontier.MOD_ID, "deep_space"));
 
 	public static double getSpaceTransitionFactor(@Nullable LocalPlayer player) {
 		if (player == null) {
 			return 0;
+		}
+
+		ResourceKey<Level> dimensionKey = player.level().dimension();
+		if (dimensionKey == DEEP_SPACE) {
+			return 1;
 		}
 
 		double height = player.getEyeY();
